35ddbdefed86f7a9a206057d447e9a9573422cfe
SkizoGG <joancolmenero@gmail.com>
2015-10-29 14:52:19 +0100

    Test

---
+++ app/src/main/java/com/appenjoyer/horizontalscrollviewwithtabs/Tab2.java
@@ -0,0 +1,61 @@
+package com.appenjoyer.horizontalscrollviewwithtabs;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Joan on 27/10/2015.
+ */
+
+
+public class Tab2 extends Fragment {
+
+    private ArrayList<dataModel> dataModelList;
+    private RecyclerView rv;
+    private MyRecyclerViewAdapter adapter;
+    private Context mContext;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
+        this.mContext = getActivity();
+
+        View rootView = inflater.inflate(R.layout.tab2, container, false);
+
+
+        rv = (RecyclerView) rootView.findViewById(R.id.rcyList);
+
+        CustomLinearLayoutManager layoutManager = new CustomLinearLayoutManager(mContext);
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        rv.setLayoutManager(layoutManager);
+
+        // Adding code here
+
+        dataModelList = new ArrayList<dataModel>();
+
+        dataModelList.add(new dataModel(ContextCompat.getDrawable(mContext, R.mipmap.ic_launcher),"Hello"));
+        dataModelList.add(new dataModel(ContextCompat.getDrawable(mContext, R.mipmap.ic_launcher),"Hello"));
+        dataModelList.add(new dataModel(ContextCompat.getDrawable(mContext, R.mipmap.ic_launcher),"Hello"));
+        dataModelList.add(new dataModel(ContextCompat.getDrawable(mContext, R.mipmap.ic_launcher),"Hello"));
+        dataModelList.add(new dataModel(ContextCompat.getDrawable(mContext, R.mipmap.ic_launcher),"Hello"));
+        dataModelList.add(new dataModel(ContextCompat.getDrawable(mContext, R.mipmap.ic_launcher),"Hello"));
+        dataModelList.add(new dataModel(ContextCompat.getDrawable(mContext, R.mipmap.ic_launcher),"Hello"));
+        dataModelList.add(new dataModel(ContextCompat.getDrawable(mContext, R.mipmap.ic_launcher),"Hello"));
+        dataModelList.add(new dataModel(ContextCompat.getDrawable(mContext, R.mipmap.ic_launcher),"Hello"));
+        adapter = new MyRecyclerViewAdapter(mContext, dataModelList);
+        rv.setAdapter(adapter);
+
+        return rootView;
+
+    }
+
+
+}
---
+++ app/src/main/java/com/appenjoyer/horizontalscrollviewwithtabs/dataModel.java
@@ -0,0 +1,19 @@
+package com.appenjoyer.horizontalscrollviewwithtabs;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Joan on 29/10/2015.
+ */
+public class dataModel {
+    public final Drawable icon;
+    public final String title;
+
+
+    public dataModel(Drawable icon, String title) {
+        this.icon = icon;
+        this.title = title;
+
+    }
+}
+
